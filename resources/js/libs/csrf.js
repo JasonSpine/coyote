c8e0d0e332c225cf22aa883170033d686e9f1d44
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2025-03-11 11:22:26 +0100

    Remove deprecated jQuery ajax csrf token

--- resources/js/libs/csrf.js
+++ resources/js/libs/csrf.js
@@ -1,20 +1,12 @@
 import axios from "axios";
-import { SOCKET_ID } from './realtime.ts';
+import {SOCKET_ID} from './realtime.ts';
+
+const csrfToken = document
+  .querySelector('meta[name="csrf-token"]')
+  .getAttribute('content');
+setToken(csrfToken);
 
 export default function setToken(token) {
   axios.defaults.headers.common['X-CSRF-TOKEN'] = token;
   axios.defaults.headers.common['X-Socket-ID'] = SOCKET_ID;
-
-  if (window.$ !== undefined) {
-    // deprecated
-    $.ajaxSetup({
-      headers: {
-        'X-CSRF-TOKEN': token
-      }
-    });
-  }
 }
-
-const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-
-setToken(csrfToken);
