c36e75731b1e23e7e6dae269d80f8083982db520
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2025-04-10 18:06:26 +0200

    Extract Database from JobBoardGate

---
+++ neon2/app/src/Database.php
@@ -0,0 +1,27 @@
+<?php
+namespace Neon2;
+
+class Database {
+    private \PDO $pdo;
+
+    public function __construct() {
+        $file = __DIR__ . '/../database.db';
+        $this->pdo = new \PDO("sqlite:$file");
+        $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+    }
+
+    public function query(string $query, array $arguments = []): array {
+        $statement = $this->pdo->prepare($query);
+        $statement->execute($arguments);
+        return $statement->fetchAll();
+    }
+
+    public function insert(string $query, array $arguments): int {
+        $this->execute($query, $arguments);
+        return (int)$this->pdo->lastInsertId();
+    }
+
+    public function execute(string $query, array $arguments = []): void {
+        $this->pdo->prepare($query)->execute($arguments);
+    }
+}
--- neon2/app/src/JobBoard/JobBoardGate.php
+++ neon2/app/src/JobBoard/JobBoardGate.php
@@ -1,21 +1,18 @@
 <?php
 namespace Neon2\JobBoard;
 
-use PDO;
+use Neon2\Database;
 
 readonly class JobBoardGate {
-    private PDO $pdo;
+    private Database $database;
 
     public function __construct() {
-        $file = __DIR__ . '/jobBoard.dat';
-        $this->pdo = new \PDO("sqlite:$file");
-        $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-        $this->pdo->exec('CREATE TABLE IF NOT EXISTS job_offers (
+        $this->database = new Database();
+        $this->database->execute('CREATE TABLE IF NOT EXISTS job_offers (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             title TEXT NOT NULL,
             duration INTEGER NOT NULL,
-            status TEXT NOT NULL
-        )');
+            status TEXT NOT NULL);');
     }
 
     public function addJobOffer(string $title, string $plan): JobOffer {
@@ -29,16 +26,16 @@ readonly class JobBoardGate {
     }
 
     public function editJobOffer(int $jobOfferId, string $jobOfferTitle): void {
-        $this->execute('UPDATE job_offers SET title = :title WHERE id = :id;', [
+        $this->database->execute('UPDATE job_offers SET title = :title WHERE id = :id;', [
             'id'    => $jobOfferId,
             'title' => $jobOfferTitle,
         ]);
     }
 
     public function payJobOfferPayment(int $jobOfferId): void {
-        $this->execute('UPDATE job_offers SET status = :status WHERE id = :id;', [
+        $this->database->execute('UPDATE job_offers SET status = :status WHERE id = :id;', [
             'id'     => $jobOfferId,
-            'status' => $this->toSql(JobOfferStatus::Published),
+            'status' => $this->format(JobOfferStatus::Published),
         ]);
     }
 
@@ -47,42 +44,27 @@ readonly class JobBoardGate {
      */
     public function listJobOffers(): array {
         return array_map(
-            fn(array $row) => new JobOffer($row['id'], $row['title'], $row['duration'], $this->fromSql($row['status'])),
-            $this->query('SELECT id, title, duration, status FROM job_offers;'));
+            fn(array $row) => new JobOffer($row['id'], $row['title'], $row['duration'], $this->parse($row['status'])),
+            $this->database->query('SELECT id, title, duration, status FROM job_offers;'));
     }
 
     public function clear(): void {
-        $this->query('DELETE from job_offers');
+        $this->database->query('DELETE from job_offers');
     }
 
     private function insertJobOffer(JobOffer $jobOffer): int {
-        return $this->insert('INSERT INTO job_offers (title, duration, status) VALUES (:title, :duration, :status);', [
+        return $this->database->insert('INSERT INTO job_offers (title, duration, status) VALUES (:title, :duration, :status);', [
             'title'    => $jobOffer->title,
             'duration' => $jobOffer->expiresInDays,
-            'status'   => $this->toSql($jobOffer->status),
+            'status'   => $this->format($jobOffer->status),
         ]);
     }
 
-    private function query(string $query, array $arguments = []): array {
-        $statement = $this->pdo->prepare($query);
-        $statement->execute($arguments);
-        return $statement->fetchAll();
-    }
-
-    private function insert(string $query, array $arguments): int {
-        $this->execute($query, $arguments);
-        return (int)$this->pdo->lastInsertId();
-    }
-
-    private function execute(string $query, array $arguments): void {
-        $this->pdo->prepare($query)->execute($arguments);
-    }
-
-    private function toSql(JobOfferStatus $status): string {
+    private function format(JobOfferStatus $status): string {
         return $status->value;
     }
 
-    private function fromSql(string $status): JobOfferStatus {
+    private function parse(string $status): JobOfferStatus {
         return JobOfferStatus::from($status);
     }
 }
