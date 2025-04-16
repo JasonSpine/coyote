2a8a74da0d0402f1f13dcc1542d62dae14f1841f
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2025-04-16 21:24:56 +0200

    Refactor acceptance app from vanilla php to laravel

--- neon2/acceptance/acceptance-app/index.php
+++ neon2/acceptance/acceptance-app/index.php
@@ -1,5 +1,9 @@
 <?php
 
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades;
+use Illuminate\Support\ServiceProvider;
 use Neon2\JobBoard;
 use Neon2\Payment\PaymentGate;
 use Neon2\Payment\PaymentService;
@@ -9,110 +13,93 @@ use Neon2\Payment\Provider\StripeWebhook;
 use Neon2\Payment\Provider\TestPaymentProvider;
 use Neon2\Payment\Provider\TestPaymentWebhook;
 
-require '../../app/vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/../../app/vendor/autoload.php';
 
-$assetName = \parse_url($_SERVER['REQUEST_URI'], \PHP_URL_PATH);
-if ($assetName === '/') {
-    $assetName = '/index.html';
-}
+class CustomServiceProvider extends ServiceProvider {
+    public function register(): void {}
 
-$gate = new PaymentGate();
-$jobBoardGate = new \Neon2\JobBoard\JobBoardGate();
-$planBundle = new JobBoard\PlanBundleGate();
-$jobBoardView = new \Neon2\JobBoardView($jobBoardGate, $planBundle);
-$board = new JobBoard($gate, $jobBoardGate, $planBundle, testMode:true);
-if ($board->testMode()) {
-    $paymentProvider = new TestPaymentProvider();
-    $paymentWebhook = new TestPaymentWebhook($paymentProvider);
-} else {
-    $paymentProvider = new Stripe('sk_test_51RBWn0Rf5n1iRahJzOJ6tJvWNO6fwKBaN7O2uVdhSGxVFVAsCeBTDgL13UWJ3VEGGLJc1ntyC5oDq5QQbVByEY8j00aluGGN0L');
-    $paymentWebhook = new StripeWebhook('whsec_W5t2VrjF8hVHk3Fp6bM4scZ5HyX9y4xB');
-}
-$paymentService = new PaymentService($gate, $paymentProvider);
+    public function boot(): void {
+        $gate = new PaymentGate();
+        $jobBoardGate = new \Neon2\JobBoard\JobBoardGate();
+        $planBundle = new JobBoard\PlanBundleGate();
+        $jobBoardView = new \Neon2\JobBoardView($jobBoardGate, $planBundle);
+        $board = new JobBoard($gate, $jobBoardGate, $planBundle, testMode:true);
+        if ($board->testMode()) {
+            $paymentProvider = new TestPaymentProvider();
+            $paymentWebhook = new TestPaymentWebhook($paymentProvider);
+        } else {
+            $paymentProvider = new Stripe('sk_test_51RBWn0Rf5n1iRahJzOJ6tJvWNO6fwKBaN7O2uVdhSGxVFVAsCeBTDgL13UWJ3VEGGLJc1ntyC5oDq5QQbVByEY8j00aluGGN0L');
+            $paymentWebhook = new StripeWebhook('whsec_W5t2VrjF8hVHk3Fp6bM4scZ5HyX9y4xB');
+        }
+        $paymentService = new PaymentService($gate, $paymentProvider);
 
-if (($_SERVER['CONTENT_TYPE'] ?? null) === 'application/json') {
-    $body = \json_decode(\file_get_contents('php://input'), true);
-} else {
-    $body = [];
-}
-if ($assetName === '/neon2/job-offers' && $_SERVER['REQUEST_METHOD'] === 'POST') {
-    $createdJobOffer = $board->addJobOffer($body['jobOfferTitle'], $body['jobOfferPlan']);
-    \http_response_code(201);
-    \header('Content-Type: application/json');
-    echo \json_encode($createdJobOffer);
-    return;
-}
-
-if ($assetName === '/neon2/job-offers/payment' && $_SERVER['REQUEST_METHOD'] === 'POST') {
-    $payment = $paymentService->preparePayment($body['userId'], 2000, $body['paymentId']);
-    \http_response_code(201);
-    \header('Content-Type: application/json');
-    echo \json_encode([
-        'providerReady' => $payment->providerReady,
-        'paymentId'     => $payment->paymentId,
-        'paymentToken'  => $payment->paymentToken,
-    ]);
-    return;
-}
-
-if ($assetName === '/neon2/job-offers' && $_SERVER['REQUEST_METHOD'] === 'PATCH') {
-    $jobBoardGate->editJobOffer($body['jobOfferId'], $body['jobOfferTitle']);
-    \http_response_code(201);
-    \header('Content-Type: application/json');
-    echo \json_encode([], \JSON_FORCE_OBJECT);
-    return;
-}
-
-if ($assetName === '/neon2/job-offers/redeem-bundle' && $_SERVER['REQUEST_METHOD'] === 'POST') {
-    $board->publishJobOfferUsingBundle($body['jobOfferId'], $body['userId']);
-    \http_response_code(201);
-    \header('Content-Type: application/json');
-    echo \json_encode([], \JSON_FORCE_OBJECT);
-    return;
-}
-
-if ($assetName === '/neon2/webhook' && $_SERVER['REQUEST_METHOD'] === 'POST') {
-    $paymentUpdate = $paymentWebhook->acceptPaymentUpdate(
-        \file_get_contents('php://input'),
-        $_SERVER['HTTP_STRIPE_SIGNATURE'] ?? '');
-    if ($paymentUpdate) {
-        $board->paymentUpdate($paymentUpdate);
+        Facades\Route::post('/neon2/job-offers', function () use ($board) {
+            $createdJobOffer = $board->addJobOffer(request()->get('jobOfferTitle'), request()->get('jobOfferPlan'));
+            return response()->json($createdJobOffer, status:201);
+        });
+        Facades\Route::post('/neon2/job-offers/payment', function () use ($paymentService) {
+            $payment = $paymentService->preparePayment(request()->get('userId'), 2000, request()->get('paymentId'));
+            return response()->json([
+                'providerReady' => $payment->providerReady,
+                'paymentId'     => $payment->paymentId,
+                'paymentToken'  => $payment->paymentToken,
+            ], status:201);
+        });
+        Facades\Route::patch('/neon2/job-offers', function () use ($jobBoardGate) {
+            $jobBoardGate->editJobOffer(request()->get('jobOfferId'), request()->get('jobOfferTitle'));
+            return response()->json([], status:201);
+        });
+        Facades\Route::post('/neon2/job-offers/redeem-bundle', function () use ($board) {
+            $board->publishJobOfferUsingBundle(request()->get('jobOfferId'), request()->get('userId'));
+            return response()->json([], status:201);
+        });
+        Facades\Route::post('/neon2/webhook', function () use ($paymentWebhook, $board) {
+            $paymentUpdate = $paymentWebhook->acceptPaymentUpdate(
+                \file_get_contents('php://input'),
+                $_SERVER['HTTP_STRIPE_SIGNATURE'] ?? '');
+            if ($paymentUpdate) {
+                $board->paymentUpdate($paymentUpdate);
+            }
+        });
+        Facades\Route::get('/neon2/status', function () use ($gate) {
+            $status = $gate->readPaymentStatus(request()->query->get('paymentId'));
+            return response()->json(match ($status) {
+                PaymentStatus::Completed => 'paymentComplete',
+                PaymentStatus::Failed    => 'paymentFailed',
+                PaymentStatus::Awaiting  => 'awaitingPayment',
+            });
+        });
+        Facades\Route::get('/', function () use ($board, $jobBoardView) {
+            return $jobBoardView->jobBoardView(
+                $board->testMode(),
+                request()->query->get('userId') ?? 1);
+        });
+        Facades\Route::get('/neon2/static/assets/{assetName}', function (string $assetName) {
+            $asset = \realPath(__DIR__ . "/../../web/dist/assets/$assetName");
+            if (file_exists($asset)) {
+                return response()->file($asset);
+            }
+            abort(404);
+        });
     }
-    return;
-}
-if ($assetName === '/neon2/status' && $_SERVER['REQUEST_METHOD'] === 'GET') {
-    parse_str($_SERVER['QUERY_STRING'], $query);
-    $status = $gate->readPaymentStatus($query['paymentId']);
-    header('Content-Type: application/json');
-    echo \json_encode(match ($status) {
-        PaymentStatus::Completed => 'paymentComplete',
-        PaymentStatus::Failed    => 'paymentFailed',
-        PaymentStatus::Awaiting  => 'awaitingPayment',
-    });
-    return;
 }
 
-if ($assetName === '/index.html') {
-    parse_str($_SERVER['QUERY_STRING'], $query);
-    echo $jobBoardView->jobBoardView($board->testMode(), $query['userId'] ?? 1);
-    return;
-}
-
-$root = \realPath(__DIR__ . '/../../web/dist');
-if (\str_starts_with($assetName, '/neon2/static/')) {
-    $assetName = \substr($assetName, \strLen('/neon2/static'));
-}
-$asset = \realPath($root . $assetName);
-if ($asset === false || !str_starts_with($asset, $root) || !file_exists($asset)) {
-    \http_response_code(404);
-    exit;
-}
-\header('Content-Type: ' . mime($asset));
-\readFile($asset);
-
-function mime(string $asset): string {
-    if (\str_ends_with($asset, '.js')) {
-        return 'application/javascript';
-    }
-    return \mime_content_type($asset);
-}
+Application::configure(__DIR__ . DIRECTORY_SEPARATOR . 'laravel')
+    ->withProviders([CustomServiceProvider::class])
+    ->booted(fn() => Facades\Config::set('app.debug', true))
+    ->booted(function () {
+        $date = \date('Y-m-d.H-i-s');
+        Facades\Config::set('logging', [
+            'default'  => 'single',
+            'channels' => [
+                'single' => [
+                    'driver' => 'single',
+                    'path'   => "ERROR.$date.log",
+                ],
+            ],
+        ]);
+    })
+    ->withExceptions()
+    ->create()
+    ->handleRequest(Request::capture());
