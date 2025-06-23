138b7d40d76a240c27b47ed482489c386c9ddd79
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2025-06-23 10:51:21 +0200

    Value proposition accepts prunes guests

--- neon2/app/src/ValueProposition.php
+++ neon2/app/src/ValueProposition.php
@@ -8,6 +8,7 @@ readonly class ValueProposition {
     public function __construct(private Guest $guest, private Connection $connection) {}
 
     public function event(string $eventName, array $metadata): void {
+        $this->guest->setSetting('hasEvent', true);
         $this->connection->table('guest_events')->insert([
             'guest_id'   => $this->guest->guestId,
             'event_name' => $eventName,
