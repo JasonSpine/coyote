37f6ab827b243c2172a506eb990373f76237eb4e
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2025-07-02 12:36:07 +0200

    Hide register and login button in navigation for unauthenticated user

--- neon2/web/src/Infrastructure/Vue/NavigationView/View/MobileMenu.vue
+++ neon2/web/src/Infrastructure/Vue/NavigationView/View/MobileMenu.vue
@@ -5,17 +5,22 @@
       <div :class="navigationLinkStyle" @click="service.showPricing(); close();" v-text="'Dla pracodawców'"/>
       <div :class="navigationLinkStyle" @click="service.showForum(); close();" v-text="'Forum'"/>
       <div :class="navigationLinkStyle" @click="service.showBlog(); close();" v-text="'Mikroblogi'"/>
-      <Design.Button primary class="mt-auto w-full" @click="service.showRegister(); close();" title="Zarejestruj się"/>
-      <Design.Button primary-outline class="mt-3 w-full" @click="service.showLogin(); close();" title="Zaloguj"/>
+      <template v-if="!store.$state.isAuthenticated">
+        <Design.Button
+          primary class="mt-auto w-full" @click="service.showRegister(); close();" title="Zarejestruj się"/>
+        <Design.Button primary-outline class="mt-3 w-full" @click="service.showLogin(); close();" title="Zaloguj"/>
+      </template>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import {Design} from "../../DesignSystem/design";
+import {useNavigationStore} from "../navigationStore";
 import {useNavigationService} from "./vue";
 
 const service = useNavigationService();
+const store = useNavigationStore();
 const navigationLinkStyle = 'py-3 text-xl active:text-green2-500';
 const emit = defineEmits(['close']);
 
