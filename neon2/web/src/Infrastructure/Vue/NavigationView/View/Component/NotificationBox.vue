bbe746765f8852d9f26fd205b1320c14f5ba9d2f
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2025-07-14 19:00:10 +0200

    Style <NotificationBox/> with highlight background

--- neon2/web/src/Infrastructure/Vue/NavigationView/View/Component/NotificationBox.vue
+++ neon2/web/src/Infrastructure/Vue/NavigationView/View/Component/NotificationBox.vue
@@ -8,34 +8,28 @@
       <span class="md:cursor-pointer" v-text="'Oznacz jako przeczytane'" @click="markAllAsViewed"/>
     </div>
     <hr class="text-tile-border mt-3">
-    <div class="h-130 overflow-y-scroll" @scroll.passive="scroll">
-      <div class="pt-1 pb-3">
-        <div v-for="notification in props.notifications" class="py-2.5 mb-1 flex">
-          <a class="size-10 rounded user-avatar flex-shrink-0 mt-0.5" :href="notification.actorProfileHref">
-            <img :src="notification.actorAvatarUrl" v-if="notification.actorAvatarUrl" class="rounded"/>
-            <div v-text="notification.actorInitials" v-else class="text-center leading-10"/>
-          </a>
-          <a class="ml-2 min-w-0 flex-grow-1"
-             :class="{'text-neutral2-400':!notification.notificationHighlighted}"
-             :href="notification.contentHref">
-            <div class="xs:flex xs:justify-between text-sm">
-              <div class="flex items-center order-1 xs:order-3">
-                {{notification.notificationDate}}
-                <div
-                  class="overflow-hidden transition-[width]"
-                  :class="notification.notificationHighlighted ? 'w-4' : 'w-0'">
-                  <div class="size-2 rounded-full bg-green2-500 ml-2"/>
-                </div>
-              </div>
-              <div v-text="notification.notificationTitle" class="order-2"/>
-            </div>
-            <div v-text="notification.contentTitle" class="truncate font-medium text-green2-500"/>
-            <div v-text="notification.contentPreview" class="truncate text-sm"/>
-          </a>
-        </div>
-        <div v-if="props.notifications.length === 0" class="text-center py-8">
-          Nie masz żadnych powiadomień.
-        </div>
+    <div class="h-130 py-3 overflow-y-scroll" @scroll.passive="scroll">
+      <div v-for="notification in props.notifications"
+           class="px-3 py-2.5 mb-1 flex rounded-lg"
+           :class="{'text-green2-600 bg-green2-100 dark:text-neutral2-600 dark:bg-tile-nested':notification.notificationHighlighted}">
+        <a class="size-10 rounded user-avatar flex-shrink-0 mt-0.5" :href="notification.actorProfileHref">
+          <img :src="notification.actorAvatarUrl" v-if="notification.actorAvatarUrl" class="rounded"/>
+          <div v-text="notification.actorInitials" v-else class="text-center leading-10 border border-green2-300 rounded"/>
+        </a>
+        <a class="ml-3 min-w-0 flex-grow-1" :href="notification.contentHref">
+          <div class="xs:flex xs:justify-between text-sm">
+            <div class="flex items-center order-1 xs:order-3" v-text="notification.notificationDate"/>
+            <div v-text="notification.notificationTitle" class="order-2"/>
+          </div>
+          <div
+            class="truncate font-medium text-green2-500"
+            :class="notification.notificationHighlighted ? '' : 'dark:text-neutral2-600'"
+            v-text="notification.contentTitle"/>
+          <div v-text="notification.contentPreview" class="truncate text-sm w-17/20"/>
+        </a>
+      </div>
+      <div v-if="props.notifications.length === 0" class="text-center py-8">
+        Nie masz żadnych powiadomień.
       </div>
     </div>
   </div>
